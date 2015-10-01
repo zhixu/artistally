1644fc843bd6c5f7852c18d7341c2dcff77e1c75
Repaxan <repaxan@gmail.com>
2015-09-30 18:27:16 -0700

    added /api/convention/copyInventory

--- aa_app/views/api/convention.py
+++ aa_app/views/api/convention.py
@@ -153,3 +153,21 @@ def unsetUser(request):
     except ValidationError as e:
         return JsonResponse({"error": "invalid: %s" % ", ".join(e.message_dict.keys())}, status = 400)
     return EMPTY_JSON_200
+
+@login_required
+def copyInventory(request):
+    d = json.loads(bytes.decode(request.body))
+    u = request.user
+    try:
+        c = models.Convention.objects.get(ID = int(d["conID"]))
+        if c == models.INV_CON:
+            return JsonResponse({"error": "invalid: convention"}, status = 400)
+        for i in u.items.filter(convention = models.INV_CON):
+            nI = models.newItem(u, c, i.name, i.fandom, i.kind, i.price, i.cost, i.numSold, i.numLeft)
+            if i.image:
+                nI.setImage(i.image)
+    except models.Convention.DoesNotExist as e:
+        return JsonResponse({"error": "couldn't find the convention"}, status = 400)
+    except ValidationError as e:
+        return JsonResponse({"error": "invalid: %s" % ", ".join(e.message_dict.keys())}, status = 400)
+    return EMPTY_JSON_200
