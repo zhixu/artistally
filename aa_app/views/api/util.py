5c9bb33337409f9c89b8bf30f54b3288dbf374f4
Repaxan <repaxan@gmail.com>
2015-11-01 00:15:28 -0700

    fix for fuwa.se change

--- aa_app/views/api/util.py
+++ aa_app/views/api/util.py
@@ -14,7 +14,7 @@ def uploadFile(request):
     d = json.loads(bytes.decode(request.body))
     u = request.user
     f = base64.b64decode(d["file"])
-    r = requests.post("https://fuwa.se/api/upload", files = {"file[]": f}).json()[0]
+    r = requests.post("https://m.fuwa.se/api/upload", files = {"file[]": f}).json()[0]
     url = "https://fuwa.se" + r["url"] if r["status"] == "error" and r["error"] == "exists" else r["url"] 
     print(url)
     return JsonResponse({"url": url})
