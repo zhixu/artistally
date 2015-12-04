b852450de74cee1f2e45b41bb0e1072556d6a7c9
Zhiyuan Xu <k.zhiyuan.xu@gmail.com>
2015-12-03 18:30:47 -0800

    added review features

--- aa_app/views/api/writeup.py
+++ aa_app/views/api/writeup.py
@@ -50,3 +50,16 @@ def setReview(request):
     except ValidationError as ex:
         return JsonResponse({"error": "invalid: %s" % ", ".join(ex.message_dict.keys())}, status = 400)
     return JsonResponse({})
+
+@login_required
+def deleteReview(request):
+    d = json.loads(bytes.decode(request.body))
+    u = request.user
+    try:
+        i = models.Writeup.objects.get(ID = int(d["writeupID"]))
+        if i.user != u:
+            return JsonResponse({"error": "not your review"}, status = 400)
+        i.delete()
+    except models.Item.DoesNotExist as ex:
+        return JsonResponse({"error": "couldn't find the review"}, status = 400)
+    return JsonResponse({})
